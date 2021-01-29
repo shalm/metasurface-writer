35f665ddf1ccc3be9eb696b56128cdb9b8b7a879
Trezitorul <trezitorul@gmail.com>
2021-01-29 11:14:35 -0500

    Added pattern_writer.py
    Moved the first single reference pattern writer function to a .py file.

---
+++ pattern_writer.py
@@ -0,0 +1,17 @@
+#pattern_writer.py
+"""This file holds the core engine for the metasurface writer, and associated helper functions"""
+import gdspy
+
+def array_singleReference(Xs, Ys, reference_geometry):
+    #set up date for file name
+    #outputFileName= str(max(Xs)-min(Xs))+'by'+ str(max(Ys)-min(Ys))+ 'map of geometries'
+    # Create a cell with a component that is used repeatedly
+    array=[]
+
+    #center a reference of the geometry at each (x,y) coordinate
+    for x in Xs:
+        for y in Ys:  
+            array.append(gdspy.CellReference(reference_geometry, (x, y), magnification=1))
+    return array
+      
+
