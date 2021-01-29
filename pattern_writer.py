98a50ddf4adb88ff7b229e808374220f628960e8
mmeyers3 <mrmeyers@mit.edu>
2021-01-29 15:01:40 -0500

    Core Function for multiple geometries
    I created the core function that accepts a list of multiple geometries and maps them to coordinates. I also tested the new function in the Core Engine function jupyter notebook.

--- pattern_writer.py
+++ pattern_writer.py
@@ -14,4 +14,11 @@ def array_singleReference(Xs, Ys, reference_geometry):
             array.append(gdspy.CellReference(reference_geometry, (x, y), magnification=1))
     return array
       
-
+def array_diffGeometries(Xs, Ys, reference_geometries):
+    array=[]
+    i=0
+    for x in Xs:
+        for y in Ys:  
+            array.append(gdspy.CellReference(reference_geometries[i], (x, y), magnification=1))
+            i+=1
+    return array
