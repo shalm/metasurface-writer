d1a67ddb6c688e91d6590ae96888b3f73f3d9b9f
mmeyers3 <mrmeyers@mit.edu>
2021-01-31 12:50:50 -0500

    Update pattern_writer.py
    I reformatted the pattern_writter functions according to the PEP 8 -- Style Guide for Python Code.

--- pattern_writer.py
+++ pattern_writer.py
@@ -3,22 +3,22 @@
 import gdspy
 
 def array_singleReference(Xs, Ys, reference_geometry):
-    #set up date for file name
-    #outputFileName= str(max(Xs)-min(Xs))+'by'+ str(max(Ys)-min(Ys))+ 'map of geometries'
-    # Create a cell with a component that is used repeatedly
     array=[]
 
     #center a reference of the geometry at each (x,y) coordinate
     for x in Xs:
         for y in Ys:  
-            array.append(gdspy.CellReference(reference_geometry, (x, y), magnification=1))
+            array.append(gdspy.CellReference(
+                reference_geometry, (x, y), magnification=1))
     return array
       
 def array_diffGeometries(Xs, Ys, reference_geometries):
     array=[]
     i=0
+    #center each reference geometry at each (x,y) coordinate
     for x in Xs:
         for y in Ys:  
-            array.append(gdspy.CellReference(reference_geometries[i], (x, y), magnification=1))
+            array.append(gdspy.CellReference(
+                reference_geometries[i], (x, y), magnification=1))
             i+=1
     return array
