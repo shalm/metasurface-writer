264a6b7967ba1cea3f1be45a08e2bdd6c7b21fa9
mmeyers3 <mrmeyers@mit.edu>
2021-01-31 13:01:33 -0500

    Update pattern_writer.py
    Made a few more changes in accordance with the PEP 8 -- Style Guide for Python Code and added docstrings.

--- pattern_writer.py
+++ pattern_writer.py
@@ -3,22 +3,47 @@
 import gdspy
 
 def array_singleReference(Xs, Ys, reference_geometry):
-    array=[]
+    '''
+    Parameters
+    ----------
+    Xs : List of x values
+    Ys : List of y values
+    reference_geometry : a geometry reference cell
 
-    #center a reference of the geometry at each (x,y) coordinate
+    Returns
+    -------
+    array : an array of the geometry centered at each (x,y) coordinate
+    
+    '''
+    
+    array = []
     for x in Xs:
         for y in Ys:  
             array.append(gdspy.CellReference(
-                reference_geometry, (x, y), magnification=1))
+                reference_geometry, (x, y), 
+                magnification=1))
     return array
       
 def array_diffGeometries(Xs, Ys, reference_geometries):
-    array=[]
-    i=0
-    #center each reference geometry at each (x,y) coordinate
+    '''
+    Parameters
+    ----------
+    Xs : List of x values
+    Ys : List of y values
+    reference_geometries : List of geometry reference cells (of length X*Y)
+
+    Returns
+    -------
+    array : an array of each geometry centered at each (x,y) coordinate
+    
+    '''
+    
+    array = []
+    i = 0
     for x in Xs:
         for y in Ys:  
             array.append(gdspy.CellReference(
-                reference_geometries[i], (x, y), magnification=1))
-            i+=1
+                reference_geometries[i], (x, y), 
+                magnification=1))
+            i += 1
     return array
