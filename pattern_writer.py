d0a98313a97d6285a69c54b01e62fcfa2604f95d
mmeyers3 <mrmeyers@mit.edu>
2021-05-05 00:27:36 -0400

    pattern_writer update
    I cleaned up the code and moved it to the pattern_writer document so that all the final functions are there with doc-strings.

--- pattern_writer.py
+++ pattern_writer.py
@@ -49,31 +49,76 @@ def array_diffGeometries(Xs, Ys, reference_geometries):
             i += 1
     return array
 
-def posArrayGenerator(a, b, numA, numB, xlimit, ylimit, generator_function, placement_function):
+def posArrayGen(a,b,dim, generator_function, placement_function, pos=(0,0), prevDir= (0,0), arr=[], prev_pos=[]):
+    '''
     
-    arr=[]
-    new_x=0
-    new_y=0
-    for x in range(-numA,numA):
-        for y in range(-numB,numB):
-            vec=[(x*a[0]+y*b[0], x*a[1]+y*b[1])]
-            if vec[0][0]<= xlimit and vec[0][0]>= -xlimit and vec[0][1] <= ylimit and vec[0][1] >= -ylimit:
-                geometry_cell= generator_function(placement_function(vec[0][0], vec[0][1]))
-                arr.append(gdspy.CellReference(geometry_cell, (vec[0][0], vec[0][1]), magnification=1))
-            elif vec[0][0]>= xlimit:
-                new_x=vec[0][0]- (2*xlimit)
-                geometry_cell= generator_function(placement_function(new_x, vec[0][1]))
-                arr.append(gdspy.CellReference(geometry_cell, (new_x, vec[0][1]), magnification=1))
-            elif vec[0][0]<= -xlimit:
-                new_x=vec[0][0]+ (2*xlimit)
-                geometry_cell= generator_function(placement_function(new_x, vec[0][1]))
-                arr.append(gdspy.CellReference(geometry_cell, (new_x, vec[0][1]), magnification=1))
-            elif vec[0][1]>= ylimit:
-                new_y=vec[0][1]- (2*ylimit)
-                geometry_cell= generator_function(placement_function(vec[0][0], new_y))
-                arr.append(gdspy.CellReference(geometry_cell, (vec[0][0], new_y), magnification=1))
-            elif vec[0][1]<= -ylimit:
-                new_y=vec[0][1]+ (2*ylimit)
-                geometry_cell= generator_function(placement_function(vec[0][0], new_y))
-                arr.append(gdspy.CellReference(geometry_cell, (vec[0][0], new_y), magnification=1))
+
+    Parameters
+    ----------
+    a : tuple 
+        First vector 
+    b : tuple 
+        Second vector
+    dim : tuple 
+        dimensions of the layout 
+    generator_function : function that takes in the output 
+        of the placement function and returns a gdspy shape
+    placement_function : function that takes in the point coordinates 
+        and returns a value that the generator_function takes in
+    pos : tuple of the starting coordinates, optional
+        The default is (0,0).
+    prevDir : tuple of the previous direction unit vector, optional
+        The default is (0,0).
+    arr : an array of the placed geometries, optional
+        The default is [].
+    prev_pos : a list of the previously used coordinates, optional
+        The default is [].
+
+    Returns
+    -------
+    arr : an array of all geometries that have 
+        been placed on the layout
+        
+
+    '''
+    [dimX, dimY] = dim
+    pax=(a[0]+pos[0])<=dimX/2 and (a[0] + pos[0])>=-dimX/2
+    pay=(a[1]+pos[1])<=dimY/2 and (a[1] + pos[1])>=-dimY/2
+    pa=bool(pax*pay)
+    nax=(-a[0]+pos[0])<=dimX/2 and (-a[0] + pos[0])>=-dimX/2
+    nay=(-a[1]+pos[1])<=dimY/2 and (-a[1] + pos[1])>=-dimY/2
+    na=bool(nax*nay)
+    
+    pbx=(b[0]+pos[0])<=dimX/2 and (b[0] + pos[0])>=-dimX/2
+    pby=(b[1]+pos[1])<=dimY/2 and (b[1] + pos[1])>=-dimY/2
+    pb=bool(pbx*pby)
+    nbx=(-b[0]+pos[0])<=dimX/2 and (-b[0] + pos[0])>=-dimX/2
+    nby=(-b[1]+pos[1])<=dimY/2 and (-b[1] + pos[1])>=-dimY/2
+    nb=bool(nbx*nby)
+
+    if (pos[0], pos[1]) in prev_pos:
+        return arr
+    
+    prev_pos.append((pos[0], pos[1]))
+    geometry_cell= generator_function(placement_function((pos[0], pos[1])))
+    arr.append(gdspy.CellReference(geometry_cell, (pos[0], pos[1]), magnification=1))
+
+    if pa:
+        vec=(a[0] + pos[0], a[1]+pos[1])
+        posArrayGen(a,b,dim, generator_function, placement_function, pos=vec, prevDir=(1,0), arr=arr, prev_pos=prev_pos)
+    
+    if na:
+        vec=(-a[0] + pos[0], -a[1]+pos[1])
+        posArrayGen(a,b,dim, generator_function, placement_function, pos=vec, prevDir=(-1,0), arr=arr, prev_pos=prev_pos)
+
+    if pb:
+        vec=(b[0] + pos[0], b[1]+pos[1])
+        posArrayGen(a,b,dim, generator_function, placement_function, pos=vec, prevDir=(0,1), arr=arr, prev_pos=prev_pos)
+    
+    if nb:
+        vec=(-b[0] + pos[0], -b[1]+pos[1])
+        posArrayGen(a,b,dim, generator_function, placement_function, pos=vec, prevDir=(0,-1), arr=arr, prev_pos=prev_pos)
+   
+    
     return arr
+    
